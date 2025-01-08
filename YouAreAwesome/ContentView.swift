78147a07ea2f5eb0253bb86d8f1263c397e6cbfd
Andy Topley <andy@andytopley.co.uk>
2025-01-08 12:26:23 +0000

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Andy Topley on 07/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+               
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Andy Topley on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
