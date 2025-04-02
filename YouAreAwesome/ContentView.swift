120e9e95c610c932981b9fa1a29f1e2763c538ee
Andy Topley <andy@andytopley.co.uk>
2025-04-02 12:48:52 +0100

    Added animation of message and image

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -17,31 +17,33 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("Show Message") {
                 
                 let messages = ["You are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You are Great!",
                                 "You are fantastic",
                                 "Fabulous? That's You!",
-                                "You Make Me Smile!",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Make Me Smile!"]
                 
                 message = messages[messageNumber]
                 messageNumber += 1
